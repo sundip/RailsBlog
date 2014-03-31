5edfc7c92cc25de76f916446350961056727f33e
sundip <mail@sundip.me>
2014-03-30 23:30:49 -0500

    first blogger commit, articles data table created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'aa4863bd0f64da4906743d594f9dc6606133dfb1b9b43fd9780f9a8a9626e18b4b2374253000421530d3dcbe32577fc9f70211593c49b82cf72ba9ff4aef1b7f'
